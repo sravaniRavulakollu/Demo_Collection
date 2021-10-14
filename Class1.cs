9ccde013fa78aa50f515825c7adabf5ad0df8c0d
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-14 12:02:08 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Demo_Collection
+{
+    class Class1
+    {
+
+    }
+    interface CarComparision<T>
+    {
+        string Name;
+        public DisplayResults();
+        public void Message()
+        {
+            Console.WriteLine("This msg is coming from the Interface");
+        }
+            
+    }
+    public static class Car:Car<car>
+    {
+        string CarModel { get; set; }
+        string ModelName { get; set; }
+
+        public Boolean Equals(Car,car)
+        {
+            Console.WriteLine("inside the car model");
+        }
+    }
+
+
+}
+
+
+    
+        
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+namepace Demo_Collection
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            // var MYLIST = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //foreach (var item in MYLIST) ...
+
+            //var MyFruits = new List<string> { "mango", "bananna", "grapes" };
+            //foreach(var item in MyFruits)
+            //{
+            //  Console.WriteLine(item);
+            // }
+
+            Stack<string> number = new Stack<string>();
+            number.Push("one");
+            number.Push("Two");
+            number.Push("Three");
+       
+
+
+            
+            foreach (var item in number)
+            {
+                Console.WriteLine("Following are the Contents pf Collection\t {0}\n", item);
+            }
+
+            //remove an element
+
+            Console.WriteLine("Removing  top of the stack \n {0}", number.Pop());
+
+
+
+
+
+            //Queue<int> MyQue = new Queue<int>();
+           // MyQue.Enqueue = 120;
+            //yQue.Enqueue = 220;
+            //MyQue.Enqueue = 330;
+
+           // foreach(var iteam in MyQue)
+            //{
+            //Console.WriteLine();
+           // }
+
+        Hashtable MyHashTable = new Hashtable();
+        MyHashTable.Add(01, "Delhi");
+        MyHashTable.Add(02, "Guntur");
+        MyHashTable.Add(03, "patna");
+        Console.WriteLine("Total content of the HashTable is {0}", MyHashTable.Count);
+        foreach(var item in MyHashTable)
+        {
+            Console.WriteLine("Content of the table is {0}", item);
+        }
+
+        }
+    }
+}
+
+
